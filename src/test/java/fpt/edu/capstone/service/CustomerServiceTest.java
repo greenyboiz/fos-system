98b5fe74f60003e36ae7662a637f5f6994ae0f63
HoangTienDat0703 <tiendat2000zx@gmail.com>
2022-08-02 21:25:49 +0700

    junit test fail CustomerService

--- src/test/java/fpt/edu/capstone/service/CustomerServiceTest.java
+++ src/test/java/fpt/edu/capstone/service/CustomerServiceTest.java
@@ -16,6 +16,8 @@ import org.mockito.quality.Strictness;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -40,6 +42,21 @@ public class CustomerServiceTest {
     }
 
     @Test
+    public void getAllCustomerTestFail(){
+        List<Customer> expect = new ArrayList<>();
+        expect.add(new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi"));
+
+        List<Customer> actual = new ArrayList<>();
+        actual.add(new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi"));
+
+        Mockito.when(customerRepository.findAll()).thenThrow(new NullPointerException(""));
+        NullPointerException result = assertThrows(NullPointerException.class, () -> customerService.getAllCustomer());
+
+//        List<Customer> result = customerService.getAllCustomer();
+        Assert.assertEquals("",result.getMessage());
+    }
+
+    @Test
     public void addCustomerTest(){
         Customer expect = new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi");
         Customer actual = new Customer("tang nguyet","065623266","mn@gmail.com","Ha Noi");
@@ -48,6 +65,17 @@ public class CustomerServiceTest {
         Customer result = customerService.addCustomer(actual);
 
         Assert.assertEquals(result,expect);
+    }
+
+    @Test
+    public void addCustomerTestFail(){
+        Customer expect = new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi");
+        Customer actual = new Customer("tang nguyet","065623266","mn@gmail.com","Ha Noi");
+
+        Mockito.when(customerRepository.save(actual)).thenThrow(new NullPointerException(""));
+        NullPointerException result = assertThrows(NullPointerException.class, () -> customerService.addCustomer(actual));
+
+        Assert.assertEquals("",result.getMessage());
     }
 
     @Test
@@ -67,23 +95,33 @@ public class CustomerServiceTest {
         Customer expect = new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi");
         Customer actual = new Customer(1l,"tang nguyet","065623266","mn@gmail.com","Ha Noi");
 
-        Mockito.when(customerRepository.findByCustomerId(2l)).thenReturn(null);
-        Mockito.when(customerRepository.save(actual)).thenReturn(expect);
+        Mockito.when(customerRepository.findByCustomerId(actual.getCustomerId())).thenReturn(expect);
 
-        Customer result = customerService.updateCustomer(actual);
-        Assert.assertEquals(result,expect);
+        Mockito.when(customerRepository.save(actual)).thenThrow(new NullPointerException(""));
+        NullPointerException result = assertThrows(NullPointerException.class, () -> customerService.updateCustomer(actual));
+        Assert.assertEquals("",result.getMessage());
     }
 
     @Test
     public void  deleteCustomerByIdTest(){
-
         Customer expect = new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi");
         Customer actual = new Customer(1l,"tang nguyet","065623266","mn@gmail.com","Ha Noi");
-
         Mockito.when(customerRepository.findByCustomerId(expect.getCustomerId())).thenReturn(actual);
-//        Mockito.when(dishesRepository.delete(actual);)
+        customerRepository.delete(actual);
+        Mockito.verify(customerRepository,Mockito.timeout(1)).delete(actual);
         boolean result = customerService.deleteCustomer(actual.getCustomerId());
         Assert.assertEquals(result,true);
+    }
+
+    @Test
+    public void  deleteCustomerByIdTestFail(){
+        Customer expect = new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi");
+        Customer actual = new Customer(1l,"tang nguyet","065623266","mn@gmail.com","Ha Noi");
+        Mockito.when(customerRepository.findByCustomerId(expect.getCustomerId())).thenThrow(new NullPointerException(""));
+        customerRepository.delete(actual);
+        Mockito.verify(customerRepository,Mockito.timeout(1)).delete(actual);
+        NullPointerException result = assertThrows(NullPointerException.class, () -> customerService.deleteCustomer(actual.getCustomerId()));
+        Assert.assertEquals("",result.getMessage());
     }
 
     @Test
@@ -96,6 +134,17 @@ public class CustomerServiceTest {
 
         Assert.assertEquals(result,expect);
     }
+
+    @Test
+    public void getCustomerByIdTestFail(){
+        Customer expect = new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi");
+        Customer actual = new Customer(1l,"tang nguyet","065623266","mn@gmail.com","Ha Noi");
+
+        Mockito.when(customerRepository.findByCustomerId(actual.getCustomerId())).thenThrow(new NullPointerException(""));
+        NullPointerException result = assertThrows(NullPointerException.class, () -> customerService.getCustomerById(actual.getCustomerId()));
+
+        Assert.assertEquals("",result.getMessage());
+    }
     @Test
     public void checkCustomerExistTest(){
         Customer expect = new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi");
@@ -105,6 +154,17 @@ public class CustomerServiceTest {
         boolean result = customerService.checkCustomerExist(actual);
 
         Assert.assertEquals(true,result);
+    }
+
+    @Test
+    public void checkCustomerExistTestFail(){
+        Customer expect = new Customer(1l,"minh nguyet","065623266","mn@gmail.com","Ha Noi");
+        Customer actual = new Customer(1l,"tang nguyet","065623266","mn@gmail.com","Ha Noi");
+
+        Mockito.when(customerRepository.checkContactExist(actual.getContact())).thenThrow(new NullPointerException(""));
+        NullPointerException result = assertThrows(NullPointerException.class, () -> customerService.checkCustomerExist(actual));
+
+        Assert.assertEquals("",result.getMessage());
     }
 
 }
