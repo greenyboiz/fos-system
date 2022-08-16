25dc73d5e7c040bbecdcdfe102d70689e9660816
manhnd128 <manhnd128@ghtk.co>
2022-08-16 20:30:33 +0700

    update ui dashboard

--- modules/report/index.vue
+++ modules/report/index.vue
@@ -2,29 +2,55 @@
   <div class="report">
     <div class="report__wrapper">
       <div class="top-report">
-        <div class="column-chart d-flex">
-          <VueApexchart
-            :key="revenueKey"
-            type="bar"
-            height="350"
-            width="50%"
-            :options="chartOptionsRV"
-            :series="seriesRV"
-          ></VueApexchart>
+        <div class="column-chart">
+          <div class="donut-top">DOANH THU</div>
+          <div class="column-bot">
+            <div class="column-left">
+              <VueApexchart
+                :key="revenueKey"
+                type="bar"
+                height="350"
+                width="100%"
+                :options="chartOptionsRV"
+                :series="seriesRV"
+              ></VueApexchart>
+            </div>
+
+            <div class="column-right">
+              <div v-for="(item, idx) in listTotal" :key="'totel' + idx" class="total">
+                <div class="total__money">{{ item.data }} VND</div>
+                <div class="total__name">{{ item.name }}</div>
+              </div>
+            </div>
+          </div>
         </div>
+
         <div class="donut-chart">
-          <VueApexchart
-            :key="costKey"
-            type="pie"
-            height="350"
-            width="50%"
-            :options="chartOptionsC"
-            :series="seriesC"
-          ></VueApexchart>
+          <div class="donut-top">CHI PHÍ</div>
+          <div class="donut-bot">
+            <div>
+              <VueApexchart
+                :key="costKey"
+                type="pie"
+                width="100%"
+                :options="chartOptionsC"
+                :series="seriesC"
+              ></VueApexchart>
+            </div>
+            <div class="legend">
+              <div v-for="item in labelsC" :key="'box' + item" class="legend__item">
+                <div class="legend__box"></div>
+                {{ item }}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div class="mid-report"></div>
-      <div class="bottom-report"></div>
+      <div class="bottom-report">
+        <div class="donut-top">LỢI NHUẬN</div>
+        <VueApexchart :key="botkey" height="400" width="100%" :options="chartArea" :series="seriesArea"></VueApexchart>
+      </div>
     </div>
   </div>
 </template>
@@ -32,7 +58,7 @@
 <script>
 import VueApexchart from 'vue-apexcharts';
 import { chartService } from '@/services';
-import { map } from 'lodash';
+import { map, reduce } from 'lodash';
 export default {
   name: 'Report',
 
@@ -42,39 +68,109 @@ export default {
 
   data() {
     return {
-      seriesRV: [{
-        name: 'Revenue',
-        data: []
-      }],
+      seriesRV: [
+        {
+          data: [],
+        },
+      ],
       chartOptionsRV: {
         chart: {
-          id: 'basic-bar'
+          id: 'basic-bar',
+          toolbar: {
+            show: false,
+          },
+        },
+        plotOptions: {
+          bar: {
+            columnWidth: '45%',
+            distributed: true,
+          },
+        },
+        dataLabels: {
+          enabled: false,
+        },
+        colors: ['#259062'],
+        legend: {
+          show: false,
         },
         xaxis: {
-          categories: []
-        }
+          categories: [],
+        },
       },
       revenueKey: 0,
+      botkey: 12,
       costKey: 0,
       seriesC: [44, 55, 13, 43, 22],
-          chartOptionsC: {
-            chart: {
-              width: 380,
-              type: 'pie',
-            },
-            labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
-            responsive: [{
-              breakpoint: 480,
-              options: {
-                chart: {
-                  width: 200
-                },
-                legend: {
-                  position: 'bottom'
-                }
-              }
-            }]
+      chartOptionsC: {
+        chart: {
+          type: 'pie',
+        },
+        legend: {
+          show: false,
+        },
+        colors: ['#259062', '#EEBF5C', '#E9693D', '#D2494D', '#AAA1A1'],
+        labels: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+      },
+      labelsC: ['Team A', 'Team B', 'Team C', 'Team D', 'Team E'],
+
+      seriesArea: [
+        {
+          name: '',
+          data: [],
+        },
+      ],
+      chartArea: {
+        chart: {
+          type: 'area',
+          zoom: {
+            enabled: false,
           },
+          toolbar: {
+            show: false,
+          },
+        },
+        markers: {
+          size: 5,
+        },
+        colors: ['#6FCF97'],
+        dataLabels: {
+          enabled: false,
+        },
+        grid: {
+          strokeDashArray: 6,
+        },
+        stroke: {
+          curve: 'straight',
+          width: 1.5,
+        },
+        xaxis: {
+          labels: {
+            show: true,
+            hideOverlappingLabels: true,
+            trim: true,
+            style: {
+              colors: '#7A7A7A',
+            },
+          },
+          tickPlacement: 'between',
+          axisTicks: {
+            show: false,
+          },
+        },
+        yaxis: {
+          opposite: false,
+          labels: {
+            show: true,
+            style: {
+              colors: '#7A7A7A',
+            },
+          },
+        },
+        legend: {
+          horizontalAlign: 'left',
+        },
+      },
+      listTotal: [],
     };
   },
 
@@ -85,7 +181,7 @@ export default {
         // this.getChartData();
       },
       deep: true,
-    }
+    },
   },
 
   mounted() {
@@ -110,13 +206,53 @@ export default {
         const seriesCost = map(res.data, (item) => {
           return item.cost;
         });
+
+        const seriesProfit = map(res.data, (item) => {
+          return item.profit;
+        });
         this.seriesRV[0].data = seriesData;
         this.chartOptionsRV.xaxis.categories = xasisData;
         this.seriesC = seriesCost;
+        this.seriesArea[0].data = seriesProfit;
+
+        const listTotal = [
+          {
+            data: reduce(
+              seriesData,
+              (sum, n) => {
+                return sum + n;
+              },
+              0
+            ),
+            name: 'Doanh thu',
+          },
+          {
+            data: reduce(
+              seriesCost,
+              (sum, n) => {
+                return sum + n;
+              },
+              0
+            ),
+            name: 'Chi phí',
+          },
+          {
+            data: reduce(
+              seriesProfit,
+              (sum, n) => {
+                return sum + n;
+              },
+              0
+            ),
+            name: 'Lợi nhuận',
+          },
+        ];
+
+        this.listTotal = listTotal;
       }
     },
   },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped src="./styles.scss"></style>
