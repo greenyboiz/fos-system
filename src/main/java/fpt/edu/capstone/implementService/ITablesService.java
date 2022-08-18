c4b24c0786583965b53978d764e01413cea76484
HoangTienDat0703 <tiendat2000zx@gmail.com>
2022-08-18 20:02:01 +0700

    fix buile heroku

--- src/main/java/fpt/edu/capstone/implementService/ITablesService.java
+++ src/main/java/fpt/edu/capstone/implementService/ITablesService.java
@@ -1,12 +1,8 @@
 package fpt.edu.capstone.implementService;
 
 import fpt.edu.capstone.entities.Tables;
-import fpt.edu.capstone.response.ResponseObject;
-import javafx.scene.control.Tab;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.persistence.Table;
+
 import java.util.List;
 
 public interface ITablesService {
