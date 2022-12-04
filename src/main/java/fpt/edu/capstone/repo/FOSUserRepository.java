dcfd590bb0891034005ef427e84aa4c9ace6e2b2
HoangTienDat0703 <tiendat2000zx@gmail.com>
2022-12-04 17:31:31 +0700

    last commit

--- src/main/java/fpt/edu/capstone/repo/FOSUserRepository.java
+++ src/main/java/fpt/edu/capstone/repo/FOSUserRepository.java
@@ -5,17 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface FOSUserRepository extends JpaRepository<FOSUser, Long> {
 
     @Query("select c from FOSUser c where c.userName = ?1 or c.contact = ?2 or c.email = ?3")
     FOSUser checkExistUserByUserNameAndContactAndEmail(String userName,String contact, String email);
-
-
     FOSUser findByUserName(String username);
-
     @Query("select c from FOSUser c where c.userId = ?1")
     FOSUser findFOSUserById(Long id);
 
